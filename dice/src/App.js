593e1742770bcf6af8855baeb31befadc4b3ad61
Morgan Williamson <morgantaylorwilliamson@protonmail.com>
2021-01-27 22:11:53 -0600

    Set up component skeletons.

--- dice/src/App.js
+++ dice/src/App.js
@@ -1,10 +1,14 @@
 import './stylesheets/App.css';
+import Header from "./components/Header";
 import Roller from "./components/DiceRolling";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Roller />
+      <Footer />
     </div>
   );
 }
---
+++ dice/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <div className="header">
+            <p>D&D Dice Roller</p>
+        </div>
+    )
+}
+
+export default Header;
