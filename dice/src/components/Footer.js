495e090dec5bf672f5907cd494e417c46a2f682f
Morgan Williamson <morgantaylorwilliamson@protonmail.com>
2021-02-13 16:56:51 -0600

    Added GH link/icon.

--- dice/src/components/Footer.js
+++ dice/src/components/Footer.js
@@ -1,9 +1,12 @@
 import React from "react";
+import "../stylesheets/Footer.css"
 
 const Footer = () => {
     return (
         <div className="footer">
-            <p>page info</p>
+            <a href="https://github.com/MorganWilliamson/dice-roll" target="_blank" rel="noreferrer">
+                <img alt="github logo" src="https://simpleicons.org/icons/github.svg"/>
+            </a>
         </div>
     )
 }
